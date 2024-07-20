9b0452a870b88eb6a5ce81590cede918d5716556
sattarawut <panpon9988@gmail.com>
2024-07-21 01:46:20 +0700

    add sort

--- datas/api.py
+++ datas/api.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 def get_std_2566():
     df = pd.read_json("https://gpa.obec.go.th/reportdata/pp3-4_2566_province.json")
+    df = df.sort_values(by=["totalstd"], ascending=False)
     return df
